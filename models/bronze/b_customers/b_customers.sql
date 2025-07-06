f785bc362f87398497be76c042942d0ef2c74ab1
Leandro Jukemura <leandro.jukemura@buser.com.br>
2025-07-06 15:48:09 -0300

    add table and ajusta ci

--- models/bronze/b_customers/b_customers.sql
+++ models/bronze/b_customers/b_customers.sql
@@ -1,10 +1,9 @@
 SELECT
   customer_id,
-  customer_name,
-  MIN(updated_at) AS created_at,
-  MAX(updated_at) AS updated_at,
+  MIN(customer_name) OVER(PARTITION BY customer_id ORDER BY updated_at DESC) AS customer_name,
+  MIN(updated_at) OVER(PARTITION BY customer_id) AS created_at,
+  MAX(updated_at) OVER(PARTITION BY customer_id) AS last_updated_at,
   COUNT(*) OVER(PARTITION BY customer_id) AS total_changes
 FROM
-  {{ source('sources', 'customers') }}
-GROUP BY customer_id, customer_name
+  `dbt-felix`.`sources`.`customers`
 QUALIFY ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY updated_at DESC) = 1
